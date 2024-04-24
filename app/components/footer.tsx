94f6fa74486a00f62c15be8a8996b242cd6c7fdd
HansM7 <melchorhans5@gmail.com>
2024-04-24 17:26:54 -0500

    Change data footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -7,28 +7,31 @@ function Footer() {
     <footer className="bg-white xl:px-44 md:px-28  px-8 flex  py-16  w-full flex-col">
       <div className="text-sm grid grid-cols-4 gap-16 justify-between w-full mb-4">
         <div className="col-span-2 md:col-span-1">
-          <span className="text-3xl">GATO</span>
+          <span className="text-3xl font-semibold">GATO</span>
         </div>
         <div className="flex flex-col col-span-2 md:col-span-1">
           <span className="font-semibold">Visitanos</span>
-          <span className="text-slate-600">Lima, Perú</span>
+          <span className="text-slate-600">Av. Brasil 2479, Jesús María</span>
         </div>
         <div className="flex flex-col col-span-2 md:col-span-1">
           <span className="font-semibold">Hablemos</span>
           <Link href={"tel:+51 976587365"} className="text-slate-600">
-            +51 946380310
+            +51 980005257
           </Link>
           <Link href={"tel:+51 976587365"} className="text-slate-600">
-            +51 946380310
+            +51 952341679
           </Link>
         </div>
         <div className="flex flex-col col-span-2 md:col-span-1">
           <span className="font-semibold">Escríbenos</span>
           <Link href={"mailto:info@gmail.com"} className="text-slate-600">
-            info@gmail.com
+            info@geniusperu.com
           </Link>
           <Link href={"mailto:info@gmail.com"} className="text-slate-600">
-            info@gmail.com
+            dortega@geniusperu.com
+          </Link>
+          <Link href={"mailto:info@gmail.com"} className="text-slate-600">
+            mjara@geniusperu.com
           </Link>
         </div>
       </div>
