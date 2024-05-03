f404226fd798f77952c2771ba8b7a457f75f4e76
HansM7 <melchorhans5@gmail.com>
2024-05-03 16:48:56 -0500

    change info in drawer

--- app/components/drawer.tsx
+++ app/components/drawer.tsx
@@ -170,11 +170,11 @@ function Drawer() {
           <div className="flex flex-col gap-4 text-sm">
             <div className="flex flex-col">
               <span className="font-semibold">Escríbenos</span>
-              <span>gato@gmail.com</span>
+              <span>info@geniusperu.com</span>
             </div>
             <div className="flex flex-col">
               <span className="font-semibold">Háblanos</span>
-              <span>9907676768</span>
+              <span>980005257</span>
             </div>
           </div>
         </div>
